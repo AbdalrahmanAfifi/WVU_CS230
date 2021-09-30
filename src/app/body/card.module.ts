336f1b33f271dab4f4869a7783d31bf913f32021
AbdalrahmanAfifi <aga0016@mix.wvu.edu>
2021-09-30 18:32:01 -0400

    updated files

--- src/app/body/card.module.ts
+++ src/app/body/card.module.ts
@@ -1,13 +1,14 @@
 export class Card {
     title: string;
     price: string;
-    imagePath:string;
+    imagePath: string;
+    
 
 
-    constructor({ title, price,imagePath }:
-        { title: string, price: string,imagePath:string }) {
+    constructor({ title, price, imagePath }:
+        { title: string, price: string, imagePath: string }) {
         this.title = title;
         this.price = price;
-        this.imagePath=imagePath;
+        this.imagePath = imagePath;
     }
 }
